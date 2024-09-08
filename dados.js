33a46dcc60c3e0f6e027fa22d20bf737f17c3b55
Thamires <121324998+ranpoluv@users.noreply.github.com>
2024-09-08 19:38:23 -0300

    Arquivo dos Dados JavaScript

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz conquistou a primeira medalha de ouro do Brasil nas Olimpíadas de 2024 em Paris e com isso se tornou a primeira mulher a conquistar uma medalha de ouro Olímpica na história do Esporte Clube Pinheiros.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Ana Patrícia Ramos",
+        descricao: "Ana Patrícia Silva Ramos é uma voleibolista e campeã olímpica brasileira praticante da modalidade de vôlei de praia e ex-voleibolista indoor.",
+        link: "https://pt.wikipedia.org/wiki/Ana_Patr%C3%ADcia_Ramos"
+    },
+    {
+        titulo: "Duda Lisboa",
+        descricao: "Eduarda dos Santos Lisboa é uma voleibolista e campeã olímpica brasileira praticante da modalidade de vôlei de praia sagrou-se campeã do Campeonato Mundial Escolar de 2013 na Itália, tricampeã consecutivamente do Campeonato Mundial de Vôlei de Praia Sub-19 nos anos de 2013,2014 e 2016",
+    link: "https://pt.wikipedia.org/wiki/Duda_Lisboa"
+    }
+];
